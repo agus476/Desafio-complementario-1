b96dda75638acc87b45a5bd54de529e3a5ba7ec1
Agustin Sanchez <agu.sanchez15@hotmail.com>
2022-04-22 18:52:53 -0300

    All changes

---
+++ script.js
@@ -0,0 +1,17 @@
+let askNumber = Number(prompt("Ingresa un numero"))
+let counter = Number (prompt("Ingresa el numero que le quieras sumar 10 veces!"))
+
+
+for (let i = 0 ; i < 11 ; i++) {
+  
+    let result= askNumber + counter * i ;
+
+    
+alert ( result)
+
+
+}
+
+ 
+
+
